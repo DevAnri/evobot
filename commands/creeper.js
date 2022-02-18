afc4019279ec21a776dc92239d87ac4dd7d3a9ca
DevAnri <korewapanchiko@gmail.com>
2022-02-18 01:14:34 -0500

    fucking work

--- commands/creeper.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
-
-client.on("message", msg => {
-    if (msg.content === "creeper","Creeper") {
-      msg.reply("aww man!");
-    }
-  })
