5ede46956418596c45f0193d9b8f0bc7b49cb055
DevAnri <korewapanchiko@gmail.com>
2022-03-30 17:17:58 -0400

    mindcrafting

--- commands/creeper.js
+++ commands/creeper.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 
 client.on("message", msg => {
-    if (msg.content === "creeper","Creeper") {
+  if (msg.content === "creeper" || msg.content === "Creeper") {
       msg.reply("aww man!");
     }
   })
